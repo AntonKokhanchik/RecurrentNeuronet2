70e9e95119548c245877f9545c27d02e9af737e7
Anton Kokhanchik <kokhanchik.tversu@gmail.com>
2018-06-13 18:55:26 +0300

    save/load feature added

--- RecurrentNeuronet2/Form1.Designer.cs
+++ RecurrentNeuronet2/Form1.Designer.cs
@@ -29,7 +29,7 @@
 		private void InitializeComponent()
 		{
 			this.buttonSelectFile = new System.Windows.Forms.Button();
-			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.openFileDialogInput = new System.Windows.Forms.OpenFileDialog();
 			this.buttonLearn = new System.Windows.Forms.Button();
 			this.textBoxEpsilon = new System.Windows.Forms.TextBox();
 			this.textBoxInnerLength = new System.Windows.Forms.NumericUpDown();
@@ -43,17 +43,21 @@
 			this.label5 = new System.Windows.Forms.Label();
 			this.textBoxAlpha = new System.Windows.Forms.TextBox();
 			this.buttonExitFile = new System.Windows.Forms.Button();
-			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.saveFileDialogResults = new System.Windows.Forms.SaveFileDialog();
 			this.textBoxTime = new System.Windows.Forms.TextBox();
 			this.label6 = new System.Windows.Forms.Label();
-			this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
-			this.buttonSaveDebug = new System.Windows.Forms.Button();
+			this.saveFileDialogLog = new System.Windows.Forms.SaveFileDialog();
+			this.buttonSaveLog = new System.Windows.Forms.Button();
+			this.buttonLoadNet = new System.Windows.Forms.Button();
+			this.buttonSaveNet = new System.Windows.Forms.Button();
+			this.saveFileDialogNet = new System.Windows.Forms.SaveFileDialog();
+			this.openFileDialogLoadNet = new System.Windows.Forms.OpenFileDialog();
 			((System.ComponentModel.ISupportInitialize)(this.textBoxInnerLength)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// buttonSelectFile
 			// 
-			this.buttonSelectFile.Location = new System.Drawing.Point(227, 12);
+			this.buttonSelectFile.Location = new System.Drawing.Point(164, 12);
 			this.buttonSelectFile.Name = "buttonSelectFile";
 			this.buttonSelectFile.Size = new System.Drawing.Size(90, 24);
 			this.buttonSelectFile.TabIndex = 0;
@@ -61,15 +65,15 @@
 			this.buttonSelectFile.UseVisualStyleBackColor = true;
 			this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
 			// 
-			// openFileDialog1
+			// openFileDialogInput
 			// 
-			this.openFileDialog1.FileName = "openFileDialog1";
-			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+			this.openFileDialogInput.FileName = "openFileDialog1";
+			this.openFileDialogInput.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
 			// 
 			// buttonLearn
 			// 
 			this.buttonLearn.Enabled = false;
-			this.buttonLearn.Location = new System.Drawing.Point(141, 141);
+			this.buttonLearn.Location = new System.Drawing.Point(78, 141);
 			this.buttonLearn.Name = "buttonLearn";
 			this.buttonLearn.Size = new System.Drawing.Size(120, 22);
 			this.buttonLearn.TabIndex = 1;
@@ -80,7 +84,7 @@
 			// textBoxEpsilon
 			// 
 			this.textBoxEpsilon.Enabled = false;
-			this.textBoxEpsilon.Location = new System.Drawing.Point(227, 68);
+			this.textBoxEpsilon.Location = new System.Drawing.Point(164, 68);
 			this.textBoxEpsilon.Name = "textBoxEpsilon";
 			this.textBoxEpsilon.Size = new System.Drawing.Size(90, 20);
 			this.textBoxEpsilon.TabIndex = 2;
@@ -89,7 +93,7 @@
 			// textBoxInnerLength
 			// 
 			this.textBoxInnerLength.Enabled = false;
-			this.textBoxInnerLength.Location = new System.Drawing.Point(258, 42);
+			this.textBoxInnerLength.Location = new System.Drawing.Point(195, 42);
 			this.textBoxInnerLength.Minimum = new decimal(new int[] {
             2,
             0,
@@ -107,7 +111,7 @@
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(72, 18);
+			this.label1.Location = new System.Drawing.Point(9, 18);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(149, 13);
 			this.label1.TabIndex = 4;
@@ -117,7 +121,7 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Enabled = false;
-			this.label2.Location = new System.Drawing.Point(72, 44);
+			this.label2.Location = new System.Drawing.Point(9, 44);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(180, 13);
 			this.label2.TabIndex = 4;
@@ -127,7 +131,7 @@
 			// 
 			this.label3.AutoSize = true;
 			this.label3.Enabled = false;
-			this.label3.Location = new System.Drawing.Point(72, 71);
+			this.label3.Location = new System.Drawing.Point(9, 71);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(57, 13);
 			this.label3.TabIndex = 4;
@@ -172,7 +176,7 @@
 			// 
 			this.label5.AutoSize = true;
 			this.label5.Enabled = false;
-			this.label5.Location = new System.Drawing.Point(72, 97);
+			this.label5.Location = new System.Drawing.Point(9, 97);
 			this.label5.Name = "label5";
 			this.label5.Size = new System.Drawing.Size(112, 13);
 			this.label5.TabIndex = 4;
@@ -181,7 +185,7 @@
 			// textBoxAlpha
 			// 
 			this.textBoxAlpha.Enabled = false;
-			this.textBoxAlpha.Location = new System.Drawing.Point(227, 94);
+			this.textBoxAlpha.Location = new System.Drawing.Point(164, 94);
 			this.textBoxAlpha.Name = "textBoxAlpha";
 			this.textBoxAlpha.Size = new System.Drawing.Size(90, 20);
 			this.textBoxAlpha.TabIndex = 2;
@@ -197,14 +201,14 @@
 			this.buttonExitFile.UseVisualStyleBackColor = true;
 			this.buttonExitFile.Click += new System.EventHandler(this.buttonExitFile_Click);
 			// 
-			// saveFileDialog1
+			// saveFileDialogResults
 			// 
-			this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+			this.saveFileDialogResults.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
 			// 
 			// textBoxTime
 			// 
 			this.textBoxTime.Enabled = false;
-			this.textBoxTime.Location = new System.Drawing.Point(227, 115);
+			this.textBoxTime.Location = new System.Drawing.Point(164, 115);
 			this.textBoxTime.Name = "textBoxTime";
 			this.textBoxTime.Size = new System.Drawing.Size(90, 20);
 			this.textBoxTime.TabIndex = 2;
@@ -214,32 +218,61 @@
 			// 
 			this.label6.AutoSize = true;
 			this.label6.Enabled = false;
-			this.label6.Location = new System.Drawing.Point(72, 118);
+			this.label6.Location = new System.Drawing.Point(9, 118);
 			this.label6.Name = "label6";
 			this.label6.Size = new System.Drawing.Size(89, 13);
 			this.label6.TabIndex = 4;
 			this.label6.Text = "Время обучения";
 			// 
-			// saveFileDialog2
+			// saveFileDialogLog
 			// 
-			this.saveFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog2_FileOk);
+			this.saveFileDialogLog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog2_FileOk);
 			// 
-			// buttonSaveDebug
+			// buttonSaveLog
 			// 
-			this.buttonSaveDebug.Location = new System.Drawing.Point(205, 255);
-			this.buttonSaveDebug.Name = "buttonSaveDebug";
-			this.buttonSaveDebug.Size = new System.Drawing.Size(194, 23);
-			this.buttonSaveDebug.TabIndex = 5;
-			this.buttonSaveDebug.Text = "Записать отладочную информацию";
-			this.buttonSaveDebug.UseVisualStyleBackColor = true;
-			this.buttonSaveDebug.Click += new System.EventHandler(this.buttonSaveDebug_Click);
+			this.buttonSaveLog.Location = new System.Drawing.Point(205, 255);
+			this.buttonSaveLog.Name = "buttonSaveLog";
+			this.buttonSaveLog.Size = new System.Drawing.Size(194, 23);
+			this.buttonSaveLog.TabIndex = 5;
+			this.buttonSaveLog.Text = "Записать отладочную информацию";
+			this.buttonSaveLog.UseVisualStyleBackColor = true;
+			this.buttonSaveLog.Click += new System.EventHandler(this.buttonSaveDebug_Click);
+			// 
+			// buttonLoadNet
+			// 
+			this.buttonLoadNet.Location = new System.Drawing.Point(274, 12);
+			this.buttonLoadNet.Name = "buttonLoadNet";
+			this.buttonLoadNet.Size = new System.Drawing.Size(125, 24);
+			this.buttonLoadNet.TabIndex = 0;
+			this.buttonLoadNet.Text = "Загрузить веса";
+			this.buttonLoadNet.UseVisualStyleBackColor = true;
+			this.buttonLoadNet.Click += new System.EventHandler(this.buttonLoadNet_Click);
+			// 
+			// buttonSaveNet
+			// 
+			this.buttonSaveNet.Location = new System.Drawing.Point(274, 60);
+			this.buttonSaveNet.Name = "buttonSaveNet";
+			this.buttonSaveNet.Size = new System.Drawing.Size(125, 24);
+			this.buttonSaveNet.TabIndex = 0;
+			this.buttonSaveNet.Text = "Сохранить веса";
+			this.buttonSaveNet.UseVisualStyleBackColor = true;
+			this.buttonSaveNet.Click += new System.EventHandler(this.buttonSaveNet_Click);
+			// 
+			// saveFileDialogNet
+			// 
+			this.saveFileDialogNet.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialogNet_FileOk);
+			// 
+			// openFileDialogLoadNet
+			// 
+			this.openFileDialogLoadNet.FileName = "openFileDialog1";
+			this.openFileDialogLoadNet.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogLoadNet_FileOk);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(411, 286);
-			this.Controls.Add(this.buttonSaveDebug);
+			this.ClientSize = new System.Drawing.Size(411, 285);
+			this.Controls.Add(this.buttonSaveLog);
 			this.Controls.Add(this.label6);
 			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label3);
@@ -254,6 +287,8 @@
 			this.Controls.Add(this.textBoxEpsilon);
 			this.Controls.Add(this.buttonAnswer);
 			this.Controls.Add(this.buttonLearn);
+			this.Controls.Add(this.buttonSaveNet);
+			this.Controls.Add(this.buttonLoadNet);
 			this.Controls.Add(this.buttonExitFile);
 			this.Controls.Add(this.buttonSelectFile);
 			this.Name = "Form1";
@@ -267,7 +302,7 @@
 		#endregion
 
 		private System.Windows.Forms.Button buttonSelectFile;
-		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialogInput;
 		private System.Windows.Forms.Button buttonLearn;
 		private System.Windows.Forms.TextBox textBoxEpsilon;
 		private System.Windows.Forms.NumericUpDown textBoxInnerLength;
@@ -281,11 +316,15 @@
 		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.TextBox textBoxAlpha;
 		private System.Windows.Forms.Button buttonExitFile;
-		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialogResults;
 		private System.Windows.Forms.TextBox textBoxTime;
 		private System.Windows.Forms.Label label6;
-		private System.Windows.Forms.SaveFileDialog saveFileDialog2;
-		private System.Windows.Forms.Button buttonSaveDebug;
+		private System.Windows.Forms.SaveFileDialog saveFileDialogLog;
+		private System.Windows.Forms.Button buttonSaveLog;
+		private System.Windows.Forms.Button buttonLoadNet;
+		private System.Windows.Forms.Button buttonSaveNet;
+		private System.Windows.Forms.SaveFileDialog saveFileDialogNet;
+		private System.Windows.Forms.OpenFileDialog openFileDialogLoadNet;
 	}
 }
 
